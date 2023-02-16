7fb1929842cb20f1251e1b040ce24573203fa774
jprasad09 <phjadhav09@gmail.com>
2023-02-16 21:23:52 +0530

    resolve cors error

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ require('dotenv').config()
 
 const express = require('express')
 const mongoose = require('mongoose')
-const cors = require('cors')
 const cookieParser = require('cookie-parser')
 
 const userRoutes = require('./routes/user')
@@ -19,16 +18,11 @@ app.use((req, res, next) => {
     res.set({
         "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Methods": "*",
+        "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization"
     });
 
     next();
 });
-const corsOptions ={
-  origin:'https://ratemyfood.onrender.com', 
-  credentials:true,            //access-control-allow-credentials:true
-  optionSuccessStatus:200
-}
-// app.use(cors(corsOptions))
 app.use(cookieParser())
 app.use(express.json())
 
