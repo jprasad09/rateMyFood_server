a6e5d4ede2f7204bdf9e6b0e4a793a0badd87d51
jprasad09 <phjadhav09@gmail.com>
2023-02-23 18:12:50 +0530

    cors issue

--- index.js
+++ index.js
@@ -15,12 +15,6 @@ const authRoutes = require('./routes/auth')
 const app = express()
 
 // middleware
-const corsOptions ={
-  origin:'https://ratemyfood.onrender.com', 
-  credentials:true,            //access-control-allow-credentials:true
-  optionSuccessStatus:200
-}
-app.use(cors(corsOptions))
 app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({
